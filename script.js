3b609a027f595c4c602c471ce4e10da0ad145f23
Adhira-Mahesh <adhiramahesh357@gmail.com>
2026-04-27 09:31:12 +0530

    added new session, used tailwind

--- script.js
+++ script.js
@@ -29,4 +29,39 @@
   </div>
 </div>
   `}
-console.log(container);
+
+let reasons= document.getElementById("reasons-grid");
+reasons_array=[
+  "Free Shipping",
+  "Easy Returns",
+  "Premium Quality",
+  "Secure Payment"
+]
+reasons_description= [
+  "On orders above ₹999",
+  "7-day hassle-free returns",
+  "Carefully crafted premium products",
+  "100% safe secure checkout"
+]
+for (let i = 0; i <= 3; i++) {
+    reasons.innerHTML += `
+      <div class="card ">
+  <div class="card__shine"></div>
+  <div class="card__glow"></div>
+  <div class="card__content">
+   
+    <div style="--bg-color: #a78bfa; background: url('/images/reasons-img-${i}.png');   background-size: contain;  /* shows full image */
+  background-repeat: no-repeat;
+  background-position: center;   ;" class="card__image"></div>
+    <div class="card__text">
+      <h1 class="card__title " style = "   font-size: 1.5rem;
+  font-weight: 600;
+  line-height: 1.3;">${reasons_array[i]}</h1>
+      <h1 class="card__description" style ='  font-size: 1rem;
+  font-weight: 600;
+  line-height: 1.4;'>${reasons_description[i]}</h1>
+    </div>
+    
+  </div>
+</div>
+`}
